68257a726218735fb2c1f851757d1e4f38bb3d33
000VIJAY <vijay893689@gmail.com>
2022-05-25 20:28:52 +0530

    day-9assigment

---
+++ uc-case-9.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHoursInMonth=200
+empRatePerHr=40
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+function getWorkingHours()
+{
+case $randomcheck in
+        "$isPartTime" )
+                empHrs=4
+        ;;
+        "$isFullTime" )
+                empHrs=8
+        ;;
+        *)
+                empHrs=0
+        ;;
+esac
+}
+function getEmpWage()
+{
+        echo $(($totalEmpHr*$empRatePerHr))
+}
+while [[ $totalEmpHr -lt $maxHoursInMonth &&
+        $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        randomcheck=$((RANDOM%3))
+        getWorkingHours $randomcheck
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+        dailyWages["$totalWorkingDays"]=$(($empHrs*$empRatePerHr))
+done
+totalSalary="$( getEmpWage $totalEmpHr )"
+echo ${dailyWages[@]}
+
+echo ${!dailyWages[@]}
+echo ${#dailyWages[@]}
